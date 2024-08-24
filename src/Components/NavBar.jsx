8453d0075b2e909fa9876595a4a495a6be1e67e0
Dia <142447820+marksofdespair@users.noreply.github.com>
2024-08-23 19:10:57 -0500

    NavBar updates

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -8,8 +8,9 @@ export function NavBar() {
     <ul className="navBar">
         <Link to="/">Home</Link>
         <Link to="/Profile">Profile</Link>
-        <Link to="/Skills">Skills</Link>
-        <Link to="/Pets">Pets</Link>
+        <Link to="/Skills">Technical Skills</Link>
+        {/* <Link to="/GraphicD">Graphic Design</Link> */}
+        {/* <Link to="/Resume">Resume</Link> */}
         <Link to="/AboutMe">About Me</Link>
         <Link to="/ServiceSearch">Search for Services</Link>
     </ul>
