52801aa2192493e7555400d6aaa26043633a498c
Dia <142447820+marksofdespair@users.noreply.github.com>
2024-09-18 12:02:09 -0500

    HELP

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import { Layout } from './Layout';
 import Home from './Pages/Home';
 import UserHome from './Pages/User/UserHome';
 import ServiceSearch from './Pages/ServiceSearch'; 
-import AddService from './Pages/Provider/AddService';
-import Pets from './Pages/User/Pets';
-import AddPet from './Pages/User/AddPet';
-import Settings from './Pages/Settings'; 
+import AddService from './Pages/Provider/AddService';  
+{/* import Pets from './Pages/User/Pets'; */}
+{/* import AddPet from './Pages/User/AddPet'; */}
+{/* import Settings from './Pages/Settings';  */}
 import AboutMe from './Pages/AboutMe';
-import Profile from './Pages/Profile';
+{/*  import Profile from './Pages/Profile'; */}
 import Skills from './Pages/Skills';
 import './App.css';
 import './Layout';
@@ -25,10 +25,10 @@ const App = () => {
             <Route path="/UserHome" element={<UserHome />} /> {/* Route for UserHome */}
             <Route path="/ServiceSearch" element={<ServiceSearch />} /> {/* Route for ServiceSearch */}
             <Route path="/AddService" element={<AddService />} /> {/* Route for AddService */}
-            <Route path="/AddPet" element={<AddPet />} /> {/* Route for AddPet */}
+            {/* <Route path="/AddPet" element={<AddPet />} /> Route for AddPet */}
             <Route path="/AboutMe" element={<AboutMe />} /> {/* Route for AddService */}
-            <Route path="/Profile" element={<Profile />} />
-            <Route path="/Pets" element={<Pets />} /> {/* Route for Pets */}
+            {/* <Route path="/Profile" element={<Profile />} /> */}
+            {/* <Route path="/Pets" element={<Pets />} /> Route for Pets */}
             <Route path="/Skills" element={<Skills />} /> {/* Route for Skills */}
           </Route>
         </Routes>
